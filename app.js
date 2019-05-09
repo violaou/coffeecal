960de1c4c479b77fe4b5aa6ecd1bb596a02aba3d
violaou <ouviola77@gmail.com>
2019-05-08 23:47:00 -0400

    JSON item generation

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ console.log('Hello, World!')
 // // Console will print the message
 // console.log('Server running at http://127.0.0.1:8081/');
 
+// to run: [node app.js]
+
 const express = require('express')
 const app = express()
-// const path = require('path')
 const bodyParser = require('body-parser')
 var mongoose = require("mongoose");
+// const path = require('path')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({limit: '50mb'}))
@@ -31,7 +33,60 @@ app.listen(8000, function () {
   console.log('Example app listening on port 8000!')
 })
 
-// code used from
-// www.jenniferbland.com/saving-data-to-mongodb-database-from-node-js-application-tutorial/
+// use moongoose to connect
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/node-demo", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/coffeecal", { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // connection!
+});
+var Schema = mongoose.Schema;
+
+//TODO: probably bad design to but all the Schema and stuff here but testing right now.
+const drinkSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  name: String,
+  size: String,
+  milk: String,
+  whip: String,
+  kcal: Number
+});
+
+// NOTE: methods must be added to the schema before compiling it with mongoose.model()
+drinkSchema.methods.speak = function () {
+  var greeting = this.name
+    ? "My name is " + this.name
+    : "I don't have a name";
+  console.log(greeting);
+}
+
+drinkSchema.methods.findSimilarSize = function() {
+  return this.model('Drinks').find({ type: this.size });
+};
+
+// compliling schema into a Model, a class in which we construct documents
+var Drinks = mongoose.model('Drinks', drinkSchema);
+//document -> a drink with properties and behaviours as our Schema
+var mochafrap = new Drinks({
+  _id: new mongoose.Types.ObjectId(),
+  name: 'Mocha Frappucino',
+  size: 'Grande',
+  milk: '2%',
+  whip: 'Whip',
+  kcal: 354
+});
+//console.log(mochafrap.name); // Grande Mocha Frappucino
+//mochafrap.speak(); // "My name is Grande Mocha Frappucino"
+
+//save to db
+mochafrap.save(function (err, mochafrap) {
+  if (err) return console.error(err);
+})
+
+//sample query, find all documents with name pro that begins with mocha and returns array of Drinks to callback.
+var query = Drinks.findOne({name: /^Mocha/});
+query.select('name');
+query.exec(function(err, drink) {
+  console.log(drink);
+});
--- backend/dbparse.js
+++ backend/dbparse.js
@@ -1,52 +1,38 @@
 /*jshint esversion: 6 */
 /*
-normalmenu format;
-Starbucks Coffee Company
-Beverage Nutrition Information
+normalmenu format:
 Product Name
 Size
 Milk
 Whip
 Serving Size
 Calories
-
-Brewed Coffee - Dark Roast Short N/A N/A 236 mL 3 0.1 0 0 0 5 0 0 0 0.3 0% 0% 0% 0% 130
-Brewed Coffee - Dark Roast Venti® N/A N/A 591 mL 5 0.1 0 0 0 10 0 0 0 1 0% 0% 2% 0% 340
-Brewed Coffee Traveler - Dark Roast 1 - 236 mL serving N/A N/A 236 mL 3 0.1 0 0 0 5 0 0 0 0.3 0% 0% 0% 0% 130
-Brewed Coffee - Decaf Pike Place® Roast Tall N/A N/A 354 mL 4 0.1 0 0 0 10 0 0 0 0.5 0% 0% 0% 0% 20
-Cinnamon Dolce Latte Tall Nonfat Whip 354 mL 220 6 4 0.2 30 115 33 0 31 9 25% 0% 30% 0% 75
-Cinnamon Dolce Latte Tall 2% No Whip 354 mL 200 5 2.5 0.2 20 125 30 0 29 9 15% 0% 30% 0% 75
-
-Short N/A N/A 236 mL 3 0.1 0 0 0 5 0 0 0 0.3 0% 0% 0% 0% 130
-Short N/A N/A 236 mL 3 0.1 0 0 0 5 0 0 0 0.3 0% 0% 0% 0% 15
+Does not have Brewed Coffee Travelers or Kids sizes
 Tall N/A N/A 354 mL 4 0.1 0 0 0 10 0 0 0 0.5 0% 0% 0% 0% 20
 Tall Nonfat Whip 354 mL 220 6 4 0.2 30 115 33 0 31 9 25% 0% 30% 0% 75
 Tall 2% No Whip 354 mL 200 5 2.5 0.2 20 125 30 0 29 9 15% 0% 30% 0% 75
-
 */
+var lineReader = require('line-reader');
 
-function generateDB () {
-  // new db;
-  // file = open('menu.txt');
-  // obj_array = file.makearray('/n').map(generate_entry); //map generate entry on each element of array
-  // for drink in obj_array:
-  //   db.add(drink.id, drink);
-  // close('menu.txt');
-}
-
-// returns a drink obj with desired info per line  of drink entry
-function generateDrink (i) {
+function generateDrink (text) {
   // regex: (Trenta|Venti®|Grande|Tall|Short|N\/A|Solo|Doppio|Triple|Quad|([0-9]*ml)).* returns group after size name found
-  // ^ doesnt on this: Brewed Coffee Traveler - Dark Roast 1 - 236 mL serving N/A N/A 236 mL 3 0.1 0 0 0 5 0 0 0 0.3 0% 0% 0% 0% 130
   var regex = /(Trenta|Venti®|Grande|Tall|Short|N\/A|Solo|Doppio|Triple|Quad|([0-9]*ml)).*/g
-  var foundgroup = i.match(regex)
+  var foundgroup = text.match(regex)
   var info = foundgroup.match(/(?:\S+\s+){0}(\S+)/g)
-  var name = i.replace(foundgroup[0], '')
-  var size = info[0]
-  var milk = info[1]
-  var whip = 2 // TODO: options for whip: Whip, No Whip, N/A
-  var kcal = 2 // TODO: doesnt work b/c of whip option (N/A, whip, no whip)
+
+  var name = i.replace(foundgroup[0], '');
+  var size = info[0];
+  var milk = info[1];
+  // options for whip: Whip, No Whip, N/A
+  if (info[2] == "No"){
+    var whip = info[2]+info[3];
+    var kcal = info[4];
+  } else {
+    var whip = info[2]
+    var kcal = info[3]
+  }
   var newDrink = new Drink(name, size, milk, whip, kcal)
+  return newDrink
 }
 
 // drink object for eah type of drink
@@ -57,4 +43,17 @@ function Drink (name, size, milk, whip, kcal) {
   this.milk = milk
   this.whip = whip
   this.kcal = kcal
+}
+
+function generateJSON () {
+  var arr = []
+  lineReader.open('normal_menu.txt', function(err, reader) {
+    if (reader.hasNextLine()) {
+      reader.nextLine(function(err,line) {
+        // each line in menu txt
+        arr.push(generateDrink(line));
+      });
+    }
+});
+
 }
