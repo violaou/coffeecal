bc02a732e37e8e5092c6fe870e8e1cf1d3c0cd56
violaou <ouviola77@gmail.com>
2019-05-12 16:00:53 -0400

    change

--- backend/dbparse.js
+++ backend/dbparse.js
@@ -1,43 +1,37 @@
 /*jshint esversion: 6 */
 /*
-normalmenu format:
-Product Name
-Size
-Milk
-Whip
-Serving Size
-Calories
+normal_menu.txt format:
+Product Name Size Milk Whip Serving Size Calories etc
 Does not have Brewed Coffee Travelers or Kids sizes
-Tall N/A N/A 354 mL 4 0.1 0 0 0 10 0 0 0 0.5 0% 0% 0% 0% 20
-Tall Nonfat Whip 354 mL 220 6 4 0.2 30 115 33 0 31 9 25% 0% 30% 0% 75
-Tall 2% No Whip 354 mL 200 5 2.5 0.2 20 125 30 0 29 9 15% 0% 30% 0% 75
+ex: Cold Brewed Coffee Tall 2% No Whip 354 mL 200 5 2.5 0.2 20 125 30 0 29 9 15% 0% 30% 0% 75
 */
-var lineReader = require('line-reader');
+const readline = require('readline');
+const fs = require('fs');
 
-function generateDrink (text) {
-  // regex: (Trenta|Venti®|Grande|Tall|Short|N\/A|Solo|Doppio|Triple|Quad|([0-9]*ml)).* returns group after size name found
-  var regex = /(Trenta|Venti®|Grande|Tall|Short|N\/A|Solo|Doppio|Triple|Quad|([0-9]*ml)).*/g
-  var foundgroup = text.match(regex)
-  var info = foundgroup.match(/(?:\S+\s+){0}(\S+)/g)
+function generateDrink (paragraph) {
+  const regex = /(Trenta|Venti®|Grande|Tall|Short|N\/A|Solo|Doppio|Triple|Quad|([0-9]*ml)).*/g;
+  const lineregex = /(?:\S+\s+){0}(\S+)/g;
+  const nameregex = /.+?(?=(Trenta|Venti®|Grande|Tall|Short|N\/A|Solo|Doppio|Triple|Quad))/g;
 
-  var name = i.replace(foundgroup[0], '');
+  var info = paragraph.match(regex)[0].match(lineregex);
+  var name = paragraph.match(nameregex)[0];
+
   var size = info[0];
   var milk = info[1];
-  // options for whip: Whip, No Whip, N/A
+  // if No Whip
   if (info[2] == "No"){
     var whip = info[2]+info[3];
     var kcal = info[4];
   } else {
-    var whip = info[2]
-    var kcal = info[3]
+    var whip = info[2];
+    var kcal = info[3];
   }
-  var newDrink = new Drink(name, size, milk, whip, kcal)
-  return newDrink
+  var newDrink = new Drink(name, size, milk, whip, kcal);
+  return newDrink;
 }
 
 // drink object for eah type of drink
 function Drink (name, size, milk, whip, kcal) {
-  this.id = 0
   this.name = name
   this.size = size
   this.milk = milk
@@ -45,15 +39,23 @@ function Drink (name, size, milk, whip, kcal) {
   this.kcal = kcal
 }
 
-function generateJSON () {
-  var arr = []
-  lineReader.open('normal_menu.txt', function(err, reader) {
-    if (reader.hasNextLine()) {
-      reader.nextLine(function(err,line) {
-        // each line in menu txt
-        arr.push(generateDrink(line));
-      });
-    }
+/*process menu file line by line, create drink object into array */
+const readInterface = readline.createInterface({
+    input: fs.createReadStream('normal_menu.txt'),
+    output: process.stdout,
+    console: false
 });
 
-}
+var arr = []
+readInterface.on('line', function(line) {
+  var drink = generateDrink(line);
+  arr.push(drink);
+});
+
+/*JSON store it in file*/
+var jsonData = JSON.stringify(arr)
+fs.writeFile("test.txt", jsonData, function(err) {
+    if (err) {
+        console.log(err);
+    }
+});
