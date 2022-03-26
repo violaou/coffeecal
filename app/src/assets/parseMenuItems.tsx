00e5a714e17f53740d71108d7c62050f0e4b2651
Viola Ou <ouviola77@gmail.com>
2022-03-26 13:53:07 -0400

    updated calorie counts , added milks

--- app/src/assets/parseMenuItems.tsx
+++ app/src/assets/parseMenuItems.tsx
@@ -2,18 +2,19 @@
 /* eslint-disable no-nested-ternary */
 /**
  * Generate Drinks and store them into JSON format from the raw files.
+ * many thanks to this post for some guidance!
+ * https://old.reddit.com/r/starbucks/comments/ds3iw3/my_guide_to_calorie_counting_and_decreasing/
  */
 
 import * as lineReader from 'line-reader';
 import * as fs from 'fs';
-import * as objectHash from 'object-hash';
 
 const REGEX = /(Trenta|Venti®|Grande|Tall|Short|N\/A|Solo|Doppio|Triple|Quad|([0-9]*ml)).*/g;
 const LINE_REGEX = /(?:\S+\s+){0}(\S+)/g;
 const NAME_REGEX = /.+?(?=(Trenta|Venti®|Grande|Tall|Short|N\/A|Solo|Doppio|Triple|Quad))/g;
 
-function generateDrink(paragraph: string): Drink|undefined {
-  if (paragraph) {
+function generateDrink(paragraph: string): Drink | null {
+  if (paragraph.length > 1) {
     const infoMatch = paragraph.match(REGEX);
     const nameMatch = paragraph.match(NAME_REGEX);
     const info: RegExpMatchArray | null = infoMatch ? infoMatch[0].match(LINE_REGEX) : null;
@@ -36,9 +37,10 @@ function generateDrink(paragraph: string): Drink|undefined {
       return drink;
     }
   }
+  return null;
 }
 /* process menu file line by line, create drink object into array */
-function readIn() {
+function readIn(): void {
   const arr: Drink[] = [];
   lineReader.eachLine('./raw/normal_menu.txt', (line: string) => {
     const drink = generateDrink(line);
@@ -47,7 +49,7 @@ function readIn() {
     if (err) throw err;
     /* JSON store it in file */
     const data = JSON.stringify(arr);
-    console.log('Drinks json generated.');
+    console.log('Drinks json generated from ./raw/normal_menu.txt');
     fs.writeFile('./data/drinks.json', data, () => {
       if (err) console.log(err);
     });
